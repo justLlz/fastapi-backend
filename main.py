e82a0a5f5b784b78dd0616a022ec7d54e0008fa4
bNaii <512049503@qq.com>
2021-06-15 17:38:32 +0800

    1.fastapi demo

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from .routers.index import index_router
+from .routers.user import user_router
+
+app = FastAPI(debug=True)
+app.include_router(index_router)
+app.include_router(user_router)
+
+
+@app.get('/')
+async def func_hello():
+    return {"hello": "world"}
+
+
+@app.get('/student/{s_id}')
+async def student_info(s_id: int):
+    return {'name': 'llz', 'age': 26, 's_id': s_id}
---
+++ routers/index.py
@@ -0,0 +1,40 @@
+from functools import lru_cache
+
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from settings import config
+
+index_router = APIRouter()
+
+
+@lru_cache
+def get_setting_info():
+    return config.Settings()
+
+
+class SettingInfo(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
+
+
+@index_router.get('/setting/info')
+async def get_setting_info(setting: config.Settings = get_setting_info()):
+    return {'app_name': setting.app_name}
+
+
+"""
+(1)、如果这个参数已经在路径中被声明过，那么它就是一个路径参数。
+
+(2)、如果这个参数的类型是单类型的(如str、float、int、bool等)，那么它就是一个请求参数。
+
+(3)、如果这个参数的类型是Pydantic数据模型，那么它就被认为是Request Body参数。
+"""
+
+
+@index_router.post('/setting/{setting_type}/info')
+async def add_setting(setting_type: str, setting_info: SettingInfo):
+    request_body_data = setting_info.dict()
+    return {"setting_type": setting_type}
