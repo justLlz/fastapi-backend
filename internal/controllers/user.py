c4a3aa0443b28817feb316305e9ca2e73f32d86e
lilinze <512049503@qq.com>
2025-03-29 19:47:44 +0800

    refactor(internal/controllers/user.py): 更新 get_user_list 接口返回值处理
    - 引入 resp_200 函数以统一响应格式
    - 将 get_user_list 接口的返回值封装为统一的响应格式

--- internal/controllers/user.py
+++ internal/controllers/user.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Query, Request
 from typing import Annotated
 from internal.services.user import user_srv
+from pkg.handle_resp import resp_200
 
 router = APIRouter(prefix="/test", tags=["test"])
 
@@ -14,4 +15,4 @@ async def hello_world(request: Request):
 async def get_user_list(request: Request,
                         page: Annotated[int, Query(ge=1, le=1000)] = 1,
                         limit: Annotated[int, Query(ge=1, le=1000)] = 10):
-    return await user_srv.get_user_list(request, page, limit)
+    return resp_200(data=await user_srv.get_user_list(request, page, limit))
