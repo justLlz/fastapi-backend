db1b421dfd44dc99fe48152993ac730862695c64
lilinze <512049503@qq.com>
2025-03-29 22:52:38 +0800

    perf(pkg): 优化 JSON 序列化处理
    - 添加 UUID 类型的序列化处理
    - 更新 datetime 类型的序列化逻辑- 调整 orjson序列化选项，提高性能和准确性
    - 移除未使用的导入项
    - 优化错误提示信息

--- pkg/handle_resp.py
+++ pkg/handle_resp.py
@@ -1,11 +1,10 @@
 import datetime
+import uuid
 from decimal import Decimal
-from uuid import UUID
 
 from fastapi.responses import ORJSONResponse
 from typing import Any, Union
 from orjson import orjson
-from pydantic.v1 import BaseModel
 from starlette.status import (HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_403_FORBIDDEN,
                               HTTP_404_NOT_FOUND, HTTP_422_UNPROCESSABLE_ENTITY, HTTP_413_REQUEST_ENTITY_TOO_LARGE,
                               HTTP_500_INTERNAL_SERVER_ERROR)
@@ -24,7 +23,7 @@ class CustomORJSONResponse(ORJSONResponse):
                     return [custom_serializer(i) for i in obj]
                 case datetime.datetime() as dt:
                     # 确保 datetime 转换为 ISO 8601 格式
-                    return datetime_to_string(obj)
+                    return datetime_to_string(dt)
                 case Decimal() as dec:
                     return str(dec)  # 避免浮点数精度丢失
                 case int() as i if abs(i) >= 2 ** 53:  # 避免 JavaScript 精度问题
@@ -33,6 +32,8 @@ class CustomORJSONResponse(ORJSONResponse):
                     return list(obj)  # JSON 不支持 set，转换为 list
                 case bytes():
                     return obj.decode("utf-8", "ignore")  # 转换为字符串
+                case uuid.UUID() as u:
+                    return str(u)  # UUID 转换为字符串
                 case _:
                     return obj
 
@@ -40,10 +41,10 @@ class CustomORJSONResponse(ORJSONResponse):
             content = custom_serializer(content)
             return orjson.dumps(
                 content,
-                option=orjson.OPT_NON_STR_KEYS | orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_PASSTHROUGH_DATETIME,
+                option=orjson.OPT_NAIVE_UTC | orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_PASSTHROUGH_DATETIME | orjson.OPT_STRICT_INTEGER,
             )
         except Exception as e:
-            raise ValueError(f"JSON 序列化失败: {e}") from e
+            raise ValueError(f"CustomORJSONResponse serializer fail: {e}") from e
 
 
 # 通用响应函数，用于避免重复代码
