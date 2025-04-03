ec6b8b5aa3b6a2594469cc5464d3fd19a006dd48
lilinze <lilinze@hqshuke.com>
2025-04-03 18:15:36 +0800

    feat(internal): 添加追踪ID上下文管理
    - 新增 trace_id_context_var 用于存储追踪ID
    - 实现 get_trace_id 函数以获取请求中的追踪ID
    - 添加 set_trace_id_context_var 和 get_trace_id_context_var 函数用于设置和获取上下文中的追踪ID

--- internal/utils/context.py
+++ internal/utils/context.py
@@ -1,4 +1,8 @@
+from contextvars import ContextVar
+
 from fastapi import HTTPException, Request
+
+trace_id_context_var: ContextVar[str] = ContextVar("trace_id", default="unknown")
 
 
 def get_user_data(request: Request) -> dict:
@@ -16,3 +20,29 @@ def get_user_id(request: Request) -> int:
     if user_id is None:
         raise HTTPException(status_code=401, detail="user_id is None")
     return user_id
+
+
+def get_trace_id(request: Request):
+    """异步任务使用"""
+    trace_id = request.state.trace_id
+    if trace_id is None:
+        raise HTTPException(400, detail="trace_id is None")
+    return trace_id
+
+
+def set_trace_id_context_var(trace_id: str):
+    """设置 trace_id"""
+    trace_id_context_var.set(trace_id)
+
+
+def get_trace_id_context_var() -> str:
+    """获取 trace_id，如果未设置，则返回 None"""
+    try:
+        trace_id = trace_id_context_var.get()
+    except LookupError:
+        raise
+
+    if trace_id == "unknown":
+        raise LookupError("trace_id is unknown")
+
+    return trace_id
