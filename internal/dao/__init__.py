4efeb75ad5bad478efb441e6a167c36e31444b33
lilinze <lilinze@hqshuke.com>
2025-07-25 16:53:53 +0800

    refactor(internal/dao): 重构 DAO 初始化文件
    - 新增多个查询器方法，支持不同场景下的数据查询
    - 优化代码结构，提高可读性和可维护性
    - 引入日志记录，增强错误处理能力

--- internal/dao/__init__.py
+++ internal/dao/__init__.py
@@ -2,54 +2,96 @@
 该目录主要用于数据库操作
 """
 from fastapi import HTTPException
+from sqlalchemy import Subquery
+from sqlalchemy.orm import InstrumentedAttribute
 
 from internal.models import ModelMixin
-from internal.utils.orm_helpers import (CountBuilder, DeleteBuilder, QueryBuilder, UpdateBuilder, new_cls_querier,
+from internal.utils.mixin_type import MixinModelType
+from internal.utils.orm_helpers import (CountBuilder, QueryBuilder, UpdateBuilder, new_cls_querier,
                                         new_cls_updater,
-                                        new_counter,
-                                        new_deleter, new_ins_updater)
+                                        new_col_counter, new_counter,
+                                        new_ins_updater, new_sub_querier)
+from pkg.logger_helper import logger
 
 
 class BaseDao:
     _model_cls: type[ModelMixin] = None
 
     @property
-    def querier(self) -> QueryBuilder:
-        return new_cls_querier(self._model_cls)
+    def model_cls(self) -> type[ModelMixin]:
+        return self._model_cls
+
+    def create(self, **kwargs) -> MixinModelType:
+        return self._model_cls.create(**kwargs)
 
     @property
-    def querier_include_deleted(self) -> QueryBuilder:
+    def querier(self) -> QueryBuilder:
+        return new_cls_querier(self._model_cls, include_deleted=False).desc_(self._model_cls.updated_at)
+
+    @property
+    def querier_inc_deleted(self) -> QueryBuilder:
+        return new_cls_querier(self._model_cls, include_deleted=True).desc_(self._model_cls.updated_at)
+
+    @property
+    def querier_unsorted(self) -> QueryBuilder:
+        return new_cls_querier(self._model_cls, include_deleted=False)
+
+    @staticmethod
+    def querier_inc_deleted_unsorted(self) -> QueryBuilder:
         return new_cls_querier(self._model_cls, include_deleted=True)
 
-    def sub_querier(self):
-        pass
-
-    @property
-    def cls_updater(self) -> UpdateBuilder:
-        return new_cls_updater(self._model_cls)
+    def sub_querier(self, subquery: Subquery) -> QueryBuilder:
+        return new_sub_querier(self._model_cls, subquery=subquery)
 
     @property
     def counter(self) -> CountBuilder:
-        return new_counter(self._model_cls)
+        return new_counter(self._model_cls, include_deleted=False)
 
     @property
-    def deleter(self) -> DeleteBuilder:
-        return new_deleter(self._model_cls)
+    def counter_inc_deleted(self) -> CountBuilder:
+        return new_counter(self._model_cls, include_deleted=True)
+
+    def col_counter(self, count_column: InstrumentedAttribute, *, is_distinct: bool = False) -> CountBuilder:
+        return new_col_counter(
+            self._model_cls, count_column=count_column, is_distinct=is_distinct, include_deleted=False
+        )
+
+    @property
+    def updater(self) -> UpdateBuilder:
+        return new_cls_updater(self._model_cls)
 
     @staticmethod
-    def ins_updater(model_ins: ModelMixin):
-        return new_ins_updater(model_ins=model_ins)
+    def ins_updater(ins: ModelMixin) -> UpdateBuilder:
+        return new_ins_updater(ins)
 
-    @property
-    def model_cls(self):
-        return self._model_cls
+    async def query_by_id_or_none(
+            self,
+            primary_id: int,
+            *,
+            creator_id: int = None,
+            include_deleted: bool = False
+    ) -> MixinModelType:
+        if include_deleted:
+            querier = self.querier_inc_deleted.eq_(self._model_cls.id, primary_id)
+        else:
+            querier = self.querier.eq_(self._model_cls.id, primary_id)
+        if creator_id and self._model_cls.has_creator_id_column():
+            querier = querier.where_by(**{self._model_cls.creator_id_column_name(): creator_id})
 
-    async def get_by_oid_or_none(self, oid: int):
-        return await self.querier.eq(self._model_cls.id, oid).get_or_none()
+        return await querier.get_or_none(include_deleted=include_deleted)
 
-    async def get_by_oid_or_exec(self, oid: int):
-        ins = await self.get_by_oid_or_none(oid)
+    async def query_by_id_or_exec(
+            self,
+            primary_id: int,
+            *,
+            creator_id: int = None,
+            include_deleted: bool = False
+    ) -> MixinModelType:
+        ins = await self.query_by_id_or_none(primary_id, creator_id=creator_id, include_deleted=include_deleted)
         if not ins:
-            raise HTTPException(status_code=404, detail=f"{self._model_cls.__name__} not found for {oid}")
-
+            logger.error(f"{self._model_cls.__name__} not found for oid={primary_id}")
+            raise HTTPException(404, detail=f"{self._model_cls.__name__} not found")
         return ins
+
+    async def query_by_ids(self, ids: list[int]) -> list[MixinModelType]:
+        return await self.querier.in_(self._model_cls.id, ids).all()
